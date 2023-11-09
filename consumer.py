e504f750ef96235d8f73dff19b6d9c0501ace775
Clayton Zhang <clayton.h.zhang@gmail.com>
2023-11-09 01:38:46 -0800

    updated producer.py and consumer.py to work

--- consumer.py
+++ consumer.py
@@ -1,5 +1,3 @@
 from kafka import KafkaConsumer
 
-producer = KafkaConsumer(topic='twitch_chat', bootstrap_servers='127.0.0.1:9092', acks=1,
-                        key_serializer=lambda v:json.dumps(v).encode('utf-8'), 
-                        value_serializer=lambda v:json.dumps(v).encode('utf-8'))
+producer = KafkaConsumer('twitch_chat', bootstrap_servers='127.0.0.1:9092')
--- producer.py
+++ producer.py
@@ -18,19 +18,27 @@ token = os.environ.get('TOKEN')
 
 channel = '#boxbox'
 
-sock = socket.socket()
-sock.connect((server, port))
-sock.send(f"PASS {token}\r\n".encode())
-sock.send(f"NICK {nickname}\r\n".encode())
-sock.send(f"JOIN {channel}\r\n".encode())
-resp = sock.recv(2048).decode('utf-8')  
-
-producer.send(topic="twitch_chat", demojize(resp).encode('utf-8'))
-print(resp)
 
 
-# if resp.startswith('PING'):
-#     sock.send("PONG\n".encode('utf-8'))
-# elif len(resp) > 0:
-# producer.send()
+def get_twitch_stream():
+    sock = socket.socket()
+    sock.connect((server, port))
+    sock.send(f"PASS {token}\r\n".encode())
+    sock.send(f"NICK {nickname}\r\n".encode())
+    sock.send(f"JOIN {channel}\r\n".encode())
+    resp = sock.recv(2048).decode('utf-8')  
 
+    try:
+        while True:
+            if resp.startswith('PING'):
+                sock.send("PONG\n".encode('utf-8'))
+            elif len(resp) > 0:
+                resp = sock.recv(2048).decode('utf-8')  
+                resp_str = demojize(resp).encode('utf-8').decode('utf-8')
+                producer.send(topic="twitch_chat", value=resp_str)
+    except KeyboardInterrupt:
+        sock.close()
+        exit()
+
+if __name__ == "__main__":
+    get_twitch_stream()
