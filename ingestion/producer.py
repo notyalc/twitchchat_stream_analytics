ee54eb734c53d76cc0667f3ca5b3a7fa2da3582a
Clayton Zhang <clayton.h.zhang@gmail.com>
2023-11-13 01:17:42 -0800

    added to parse_resp function to pull out actual username from the first part of the message

--- ingestion/producer.py
+++ ingestion/producer.py
@@ -15,12 +15,20 @@ server = os.environ.get('SERVER')
 port = int(os.environ.get('PORT'))
 nickname = os.environ.get('NICKNAME')
 token = os.environ.get('TOKEN')
-channel = '#scarra'
 
-def parse_resp(s, keys = ['user', 'type', 'host']):
-    d = dict((key, value.lstrip(':')) for key, value in zip(keys, s.split()))
+channel = '#shroud' # Change the channel to choose one of choice. 
+
+def parse_resp(s, keys=['user', 'type', 'host']):
+    parts = s.split(' ')
+    username = parts[0].lstrip(':').split('!')[0]  # Extract only the username
+
+    d = dict((key, value.lstrip(':')) for key, value in zip(keys, parts))
+    d['user'] = username
     d['message'] = s.split(' :')[1].split('\r\n')[0]
+
     return d
+
+print(parse_resp(":ranran46!ranran46@ranran46.tmi.twitch.tv PRIVMSG #scarra :Gnar is such an underrated carry\r\n"))
 
 def get_twitch_stream():
     sock = socket.socket()
@@ -37,7 +45,8 @@ def get_twitch_stream():
             elif len(resp) > 0:
                 resp = sock.recv(2048).decode('utf-8')  
                 resp_str = demojize(resp).encode('utf-8').decode('utf-8')
-                producer.send(topic="twitch_chat", value=resp_str)
+                resp_json = parse_resp(resp_str)
+                producer.send(topic="twitch_chat", value=resp_json)
                 
     except KeyboardInterrupt:
         sock.close()
