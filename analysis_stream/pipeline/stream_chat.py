a87bff93d589cf9b93ce2c4ebdc775c2024763f2
James H <hill0596@gmail.com>
2023-11-14 06:57:53 -0800

    fixed redundancy

--- analysis_stream/pipeline/stream_chat.py
+++ analysis_stream/pipeline/stream_chat.py
@@ -4,9 +4,8 @@ from dotenv import load_dotenv
 from pathlib import Path
 from analysis_stream.ingestion.producer import *
 
-
 if __name__ == "__main__":
-
+    
     load_dotenv()
     server = os.environ.get('SERVER')
     port = int(os.environ.get('PORT'))
@@ -19,7 +18,7 @@ if __name__ == "__main__":
             config = yaml.safe_load(yaml_file)
     else:
         raise Exception(f'Missing {yaml_file_path} file.')
-    
+
     kp = kafka_producer(
         bootstrap_server = config.get('topic1').get('bootstrap_server'),
         server = server,
@@ -29,5 +28,4 @@ if __name__ == "__main__":
         channel = config.get('channel')
         )
     
-    if __name__ == '__main__':
-        kp.get_twitch_stream(topic = config.get('topic1').get('name'))
+    kp.get_twitch_stream(topic = config.get('topic1').get('name'))
