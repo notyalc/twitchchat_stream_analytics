5c0ac2cdc1607d362d72ea5f122ba6971a3bf882
Clayton Zhang <clayton.h.zhang@gmail.com>
2023-11-14 02:57:09 -0800

    updated producer.send to include user as the key in message

--- analysis_stream/ingestion/consumer.py
+++ analysis_stream/ingestion/consumer.py
@@ -1,3 +1,8 @@
 from kafka import KafkaConsumer
+import json
 
-producer = KafkaConsumer('twitch_chat', bootstrap_servers='127.0.0.1:9092')
+consumer = KafkaConsumer('twitch_chat', bootstrap_servers='127.0.0.1:9092', auto_offset_reset='earliest')
+for message in consumer:
+    # Assuming the message value is a JSON-formatted string
+    twitch_message = json.loads(message.value)
+    print(twitch_message)
